9038aa355605f9a00ecf14643f6a651b3029e367
Ishan Garg <igarg2001@gmail.com>
2023-06-09 20:47:37 +0530

    navbar and modal

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService],
+})
+export class ModalComponent implements OnInit {
+  constructor(public modal: ModalService) {}
+
+  ngOnInit(): void {}
+
+  closeModal(event: Event): void {
+    event.preventDefault();
+    this.modal.toggleModal();
+  }
+}
