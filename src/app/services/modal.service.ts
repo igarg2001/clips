8fd82b51be7b69cdc341ff65b8ee6bde3bd6aff6
Ishan Garg <igarg2001@gmail.com>
2023-06-10 10:55:39 +0530

    ID system for modals

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  visible: boolean;
+  id: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
-  private visible = false;
+  private modals: IModal[] = [];
   constructor() {}
 
-  isModalVisible(): boolean {
-    return this.visible;
+  registerModal(id: string): void {
+    this.modals.push({ id, visible: false });
+    // console.log(this.modals);
   }
 
-  toggleModal(): void {
-    this.visible = !this.visible;
+  isModalVisible(id: string): boolean {
+    return !!this.modals.find((modal) => modal.id === id)?.visible;
+  }
+
+  toggleModal(id: string): void {
+    const modal = this.modals.find((modal) => modal.id === id);
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
   }
 }
