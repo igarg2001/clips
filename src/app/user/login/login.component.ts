dbc7dce32f45493b268689ac25d5b2f5c39bad73
Ishan Garg <igarg2001@gmail.com>
2023-06-13 19:31:38 +0530

    login form implemented using template forms

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ICredential {
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  credentials: ICredential = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    console.log(this.credentials);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
